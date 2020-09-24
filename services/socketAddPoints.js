325383ef4dbb885ed037d21046fe22122ac0eca5
Kpoke <danielolojakpoke@gmail.com>
2020-09-25 00:56:14 +0100

    minor bug fixes

--- services/socketAddPoints.js
+++ services/socketAddPoints.js
@@ -3,7 +3,7 @@ const User = require("../model/user");
 
 module.exports = async (token) => {
   const decoded = jwt.verify(token, process.env.USERKEY);
-  const user = await User.findOne({ _id: decoded.userId });
+  const user = await User.findById(decoded.userid);
   if (!user) {
     return "User is not authorized";
   }
